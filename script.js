98f05c1120660f800d5b8c33b2d7a532adb6467c
natalia <devidvarl@gmail.com>
2022-10-20 10:48:37 +0200

    lorem

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-const line = prompt('Cut text','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi aperiam perspiciatis quaerat dolorum eveniet impedit!')
+const lorem = prompt('','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi aperiam perspiciatis quaerat dolorum eveniet impedit!')
+const line = lorem.substring(0, 5);
+console.log(lorem);
+alert(line);
