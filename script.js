42e03b5ac30ebfe8754f6bb1d9784832774f717c
natalia <devidvarl@gmail.com>
2022-10-20 10:50:46 +0200

    alert line2

--- script.js
+++ script.js
@@ -6,5 +6,6 @@ alert(line);
 const lorem2 = prompt('','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi aperiam perspiciatis quaerat dolorum eveniet impedit!')
 const line2 = lorem2.slice(-5);
 console.log(lorem2);
+alert(line2);
 
 
